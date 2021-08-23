100b423698a6b683427f856e0c9060535096fb6f
sanghangning <wb.sanghangning@mesg.corp.netease.com>
2021-08-23 10:48:27 +0800

    perf：👌 优化axios，新增终端重复请求模块

--- src/plugins/local/axios.js
+++ src/plugins/local/axios.js
@@ -1,8 +1,48 @@
 import axios from "axios";
+import qs from 'qs'
+
+// 中断重复请求
+const pendingRequest = new Map();
+
+function generateReqKey(config) {
+  const {
+    method,
+    url,
+    params,
+    data
+  } = config;
+  return [method, url, qs.stringify(params), qs.stringify(data)].join(
+    "&"
+  );
+}
+
+function addPendingRequest(config) {
+  const requestKey = generateReqKey(config);
+  config.cancelToken =
+    config.cancelToken ||
+    new axios.CancelToken((cancel) => {
+      if (!pendingRequest.has(requestKey)) {
+        pendingRequest.set(requestKey, cancel);
+      }
+    });
+}
+
+function removePendingRequest(config) {
+  const requestKey = generateReqKey(config);
+  if (pendingRequest.has(requestKey)) {
+    const cancel = pendingRequest.get(requestKey);
+    cancel(requestKey);
+    pendingRequest.delete(requestKey);
+  }
+}
+
+
 
 // http request 拦截器
 axios.interceptors.request.use(
   config => {
+    removePendingRequest(config); // 检查是否存在重复请求，若存在则取消已发的请求
+    addPendingRequest(config); // 把当前请求添加到pendingRequest对象中
     return config;
   },
   err => {
@@ -13,9 +53,16 @@ axios.interceptors.request.use(
 // http response 拦截器
 axios.interceptors.response.use(
   response => {
+    removePendingRequest(response.config); // 从pendingRequest对象中移除请求
     return response;
   },
   error => {
+    removePendingRequest(error.config || {}); // 从pendingRequest对象中移除请求
+    if (axios.isCancel(error)) {
+      console.log("已取消的重复请求：" + error.message);
+    } else {
+      // 添加异常处理
+    }
     return Promise.reject(error);
   }
 );
